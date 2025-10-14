71c3d004677cd06beac52a649cc1229601658275
unknown <ikautilyan@gmail.com>
2025-10-14 20:58:41 +0530

    changed import structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from auth import router as auth_router
-from orders import router as order_router
+from app.auth import router as auth_router
+from app.orders import router as order_router
 
 app = FastAPI()
 
@@ -9,4 +9,4 @@ app.include_router(order_router, prefix="/api", tags=["orders"])
 
 @app.get("/")
 def home():
-    return {"message": "Python server is running locally!"}
+    return {"message": "Python server is running inside Docker!"}
