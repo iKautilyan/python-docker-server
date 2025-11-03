20cdb0b67e1cac519a0db1ad96c981c712db1ac7
unknown <ikautilyan@gmail.com>
2025-11-03 15:12:12 +0530

    get login

--- app/auth.py
+++ app/auth.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 BASE_URL = os.getenv("BASE_URL")
 LOGIN_URL = f"{BASE_URL}/authentication/v1/user/session"
 
-@router.post("/login")
+@router.api_route("/login", methods=["GET", "POST"])
 def login_user():
     payload = {
         "user_id": os.getenv("USER_ID"),
@@ -27,11 +27,14 @@ def login_user():
         "x-api-key": os.getenv("API_KEY")
     }
 
-    res = requests.post(LOGIN_URL, json=payload, headers=headers)
-    data = res.json()
+    try:
+        res = requests.post(LOGIN_URL, json=payload, headers=headers)
+        data = res.json()
+        if res.status_code != 200 or "data" not in data:
+            return {"error": "Login failed", "details": data}
 
-    if res.status_code != 200 or "data" not in data:
-        return {"error": "Login failed", "details": data}
+        access_token = data["data"].get("register_token", None)
+        return {"status": "success", "access_token": access_token}
 
-    access_token = data["data"].get("register_token", None)
-    return {"status": "success", "access_token": access_token}
+    except Exception as e:
+        return {"error": str(e)}
